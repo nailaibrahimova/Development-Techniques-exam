dfa55173ca975537e86d771e056be5f623e43485
userA <userArepo>
2019-04-12 09:35:39 +0400

    main of string

---
+++ main_string.c
@@ -0,0 +1,22 @@
+#include "string.c"
+
+int main(int argc, char const *argv[])
+{
+    char *s1=calloc(100, sizeof(char));
+    printf("give the string s1:\n");
+    scanf("%s", s1);
+    //getting the length of s1
+    int n=lengthOfString(s1);
+    printf("length of |%s| is %d\n", s1, n);
+
+    // char *s2=calloc(100, sizeof(char));
+    // // s2="hello";
+    // printf("give the string s2:\n");
+    // scanf("%s", s2);
+    // //comparing 2 strings
+    // printf("compare=%d\n", compareOfStrings(s1, s2));
+
+    // //is string palindrome
+    // printf("isPalindrome=%d\n", isPalindrome(s1));
+    // return 0;
+}
